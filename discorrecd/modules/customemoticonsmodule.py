e04704bda8f3c9d218f5f9edddd3370f47cd2762
Apple <appul@users.noreply.github.com>
2016-09-25 14:09:05 +0200

    Add docstrings

--- discorrecd/modules/customemoticonsmodule.py
+++ discorrecd/modules/customemoticonsmodule.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+from typing import Tuple
 
 import discord
 
@@ -14,8 +15,16 @@ _EMOTICON_PREFIX = '/'
 
 
 class CustomEmoticonsModule(Module):
+    """Automatically replaces messages with custom images if emoticon names are found in the text.
+
+    Images are searched for in the ``/custom-emoticons`` directory relative to the directory
+    of the ``__main__`` file. If a word replaceable with an emoticon is found, the message is
+    deleted and replaced with a new message that includes the emoticon image.
+    """
+
     @Module.Event(['message'])
     async def on_message(self, message: discord.Message):
+        """Event handler for new messages"""
         if message.author.id == self.client.connection.user.id:
             word, image = self.parse_message(message.content)
             if word and image:
@@ -23,7 +32,12 @@ class CustomEmoticonsModule(Module):
                 await self.client.delete_message(message)
                 await self.client.send_file(message.channel, image, content=new_content)
 
-    def parse_message(self, message: str):
+    def parse_message(self, message: str) -> Tuple[str, str]:
+        """Parse the content of a message and finds the replaceable word-image combination
+
+        :param message: The content of a message
+        :returns: The word and the path to the image.
+        """
         for word in message.split():
             if word.startswith(_EMOTICON_PREFIX):
                 image = self.find_emoticon(word[1:])
@@ -32,7 +46,12 @@ class CustomEmoticonsModule(Module):
         return None, None
 
     @staticmethod
-    def find_emoticon(name):
+    def find_emoticon(name: str) -> str:
+        """Find an image in the emoticon directory based on a name
+
+        :param name: The name of the image
+        :return: The path to the image, None if no image was found
+        """
         for ext in _IMG_EXTENSIONS:
             path = os.path.join(_IMG_DIR, name + ext)
             if os.path.isfile(path):
